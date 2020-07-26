1030c66cb992ed9a775081cd418032761220fe28
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-26 02:51:15 -0300

    create directive project

---
+++ directive/src/app/directive-ngif/directive-ngif.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-directive-ngif',
+  templateUrl: './directive-ngif.component.html',
+  styleUrls: ['./directive-ngif.component.css']
+})
+export class DirectiveNgifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
