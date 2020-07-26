dccd5aa17ed4b7fc32433245f6cf3d8f37fbb077
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-26 01:46:56 -0300

    events with output properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  courseName: string = 'Angular';
+
   constructor() { }
 
   ngOnInit(): void {
