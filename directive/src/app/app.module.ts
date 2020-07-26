ee6e821b335a8339d87474438dee650a4d5a9897
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-26 02:59:16 -0300

    ng lint, ng test, ng e2e

--- directive/src/app/app.module.ts
+++ directive/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveNgifComponent
+    DirectiveNgifComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
